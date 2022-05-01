81d707ee5fbd62a5f4c3c92807dc96748c1ca632
Jovel Quiamco <mlph-jovel@MLPH-Jovels-MacBook-Pro.local>
2022-05-02 04:17:16 +0800

    List

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Dish> dishes = [
+      Dish('Bagnet', 'bagnet.jpeg'),
+      Dish('Chicken Bbq', 'chicken_bbq.jpeg'),
+      Dish('Chicken Curry', 'chicken_curry.jpeg'),
+      Dish('Chicken Macaroni', 'chicken_macaroni.jpeg'),
+      Dish('Pork Adobo', 'pork_adobo.jpeg')
+    ];
+
     return MaterialApp(
       title: 'Flutter Basics Exercise',
       home: Scaffold(
@@ -16,12 +24,34 @@ class MyApp extends StatelessWidget {
           title: const Text('Top 5 Favorite Dishes'),
           centerTitle: true,
         ),
-        body: ListView(
-          children: [
-            Image.asset('assets/filipino_food.jpeg')
-          ],
+        body: ListView.builder(
+          itemCount: dishes.length + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Image.asset('assets/filipino_food.jpeg');
+            }
+            return Card(
+              child: ListTile(
+                onTap: (){
+                  // TODO: show details
+                },
+                title: Text(dishes[index - 1].name),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${dishes[index - 1].image}'),
+                ),
+                trailing: const Icon(Icons.keyboard_arrow_right)
+              ),
+            );
+          },
         ),
       )
     );
   }
+}
+
+class Dish {
+  String name;
+  String image;
+
+  Dish(this.name, this.image);
 }
