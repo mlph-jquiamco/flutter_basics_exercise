f9c91f997fc82b2e3c427be4f157d935297896e4
Jovel Quiamco <mlph-jovel@MLPH-Jovels-MacBook-Pro.local>
2022-05-02 04:56:17 +0800

    Dish image

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'dish_model.dart';
 
 class Details extends StatelessWidget {
@@ -12,16 +11,18 @@ class Details extends StatelessWidget {
     final args = ModalRoute.of(context)!.settings.arguments as Dish;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: Text(args.name),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text(args.name),
-        ),
-      ),
+      body: ListView(
+        children: [
+          Image.asset(
+            'assets/${args.image}',
+            width: 800,
+            height: 400,
+            fit: BoxFit.cover,
+          )
+        ],
+      )
     );
   }
 }
