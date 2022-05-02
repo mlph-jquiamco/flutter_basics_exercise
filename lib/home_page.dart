d195bf667e46777f011b27eb8e9a62613e9ea7f0
Jovel Quiamco <mlph-jovel@MLPH-Jovels-MacBook-Pro.local>
2022-05-02 12:09:58 +0800

    Refactor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,41 +10,40 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<Dish> dishes = DishService().generateMockDishes();
 
+    List<Card> _generateDishList(List<Dish> dishes) {
+      return [for (final dish in dishes) Card(
+        child: ListTile(
+            onTap: (){
+              Navigator.pushNamed(
+                  context,
+                  Details.routeName,
+                  arguments: dish
+              );
+            },
+            title: Text(dish.name),
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/${dish.image}'),
+            ),
+            trailing: const Icon(Icons.keyboard_arrow_right)
+        ),
+      )];
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Top 5 Favorite Dishes'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: dishes.length + 1, // Include header image on count
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return Image.asset(
-              'assets/filipino_food.jpeg',
-              width: 500,
-              height: 300,
-              fit: BoxFit.cover,
-            );
-          }
-          final adjustedIndex = index - 1;
-          return Card(
-            child: ListTile(
-              onTap: (){
-                print(dishes[adjustedIndex].procedures);
-                Navigator.pushNamed(
-                  context,
-                  Details.routeName,
-                  arguments: dishes[adjustedIndex]
-                );
-              },
-              title: Text(dishes[adjustedIndex].name),
-              leading: CircleAvatar(
-                backgroundImage: AssetImage('assets/${dishes[adjustedIndex].image}'),
-              ),
-              trailing: const Icon(Icons.keyboard_arrow_right)
-            ),
-          );
-        },
+      body: ListView(
+        children: [
+          Image.asset(
+            'assets/filipino_food.jpeg',
+            width: 500,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          ..._generateDishList(dishes)
+        ],
       ),
     );
   }
